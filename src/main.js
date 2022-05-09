f177a600de9713d2c7abee2383c211cdb836a873
thanhdat.ly <thanhdat.ly@estuary.solutions>
2022-05-09 13:07:11 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import App from "./App";
+import router from "./router";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  router,
+  components: { App },
+  template: "<App/>"
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-container>
+    <b-alert show variant="primary">Primary Alert</b-alert>
+    <b-alert show variant="secondary">Secondary Alert</b-alert>
+    <b-alert show variant="success">Success Alert</b-alert>
+    <b-alert show variant="danger">Danger Alert</b-alert>
+    <b-alert show variant="warning">Warning Alert</b-alert>
+    <b-alert show variant="info">Info Alert</b-alert>
+    <b-alert show variant="light">Light Alert</b-alert>
+    <b-alert show variant="dark">Dark Alert</b-alert>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      // msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+<style scoped></style>
